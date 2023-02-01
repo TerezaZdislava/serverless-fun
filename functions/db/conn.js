5bc37b3662c91a20527ccb35ba05cbe07efc2426
TerezaZdislava <bastova.ter@gmail.com>
2023-01-31 16:17:43 -0800

    db connected 6

--- functions/db/conn.js
+++ functions/db/conn.js
@@ -1,21 +1,46 @@
-const MongoClient = require('mongodb').MongoClient;
-const MONGODB_URI =
-  'mongodb+srv://me:Teruse666@jidelnicek.djbghxk.mongodb.net/?retryWrites=true&w=majority';
-let cachedDb = null;
+// const MongoClient = require('mongodb').MongoClient;
+// const MONGODB_URI =
+//   'mongodb+srv://me:Teruse666@jidelnicek.djbghxk.mongodb.net/?retryWrites=true&w=majority';
+// let cachedDb = null;
 
-async function connectToDatabase() {
-  console.log('4');
-  if (cachedDb) {
-    console.log('cachedDb');
-    return cachedDb;
-  }
+// async function connectToDatabase() {
+//   console.log('4');
+//   if (cachedDb) {
+//     console.log('cachedDb');
+//     return cachedDb;
+//   }
 
-  const client = await MongoClient.connect(MONGODB_URI);
-  console.log('client ' + client);
-  const db = await client.db('food');
-  console.log('db ' + db);
-  cachedDb = db;
-  return db;
-}
+//   const client = await MongoClient.connect(MONGODB_URI);
+//   console.log('client ' + client);
+//   const db = await client.db('food');
+//   console.log('db ' + db);
+//   cachedDb = db;
+//   return db;
+// }
 
-exports.connectToDatabase = connectToDatabase;
+// exports.connectToDatabase = connectToDatabase;
+
+// const { MongoClient } = require("mongodb");
+// const mongoClient = new MongoClient('mongodb+srv://me:Teruse666@jidelnicek.djbghxk.mongodb.net/?retryWrites=true&w=majority');
+// const clientPromise = mongoClient.connect();
+
+// const handler = async (event) => {
+//   try {
+//       const database = (await clientPromise).db('food');
+//       const collection = database.collection('jidelnicek');
+//       const results = await collection.aggregate([
+//         { $match: { type: { $in: ['breakfast'] } } },
+//         { $match: { diets: { $in: ['vegan'] } } },
+//         { $sample: { size: 1 } },
+//       ])
+//       .toArray();
+//       return {
+//           statusCode: 200,
+//           body: JSON.stringify(results),
+//       }
+//   } catch (error) {
+//       return { statusCode: 500, body: error.toString() }
+//   }
+// }
+
+// module.exports = { handler }
--- functions/getMeals.js
+++ functions/getMeals.js
@@ -1,126 +1,160 @@
-const dbo = require('./db/conn');
+const { MongoClient } = require('mongodb');
+const mongoClient = new MongoClient(
+  'mongodb+srv://me:Teruse666@jidelnicek.djbghxk.mongodb.net/?retryWrites=true&w=majority',
+);
+const clientPromise = mongoClient.connect();
 
-exports.handler = async function (event, context, callback) {
-  console.log('1');
-  const params = JSON.parse(event.body);
-  const numberOfMeals = params.meals;
-  const diet = params.diet;
-  console.log(numberOfMeals, diet);
-  console.log('2');
-  let menu = {};
-  console.log('3');
-  const db = await dbo.connectToDatabase();
-
-  let breakfast = await db
-    .collection('meals')
-    .aggregate([
-      { $match: { type: { $in: ['breakfast'] } } },
-      { $match: { diets: { $in: [diet] } } },
-      { $sample: { size: 1 } },
-    ])
-    .toArray();
-  menu.breakfast = breakfast[0];
-  console.log('menu.breakfast ' + menu.breakfast);
-  // if (numberOfMeals === 5) {
-  //   let snack = await db
-  //     .collection('meals')
-  //     .aggregate([
-  //       { $match: { type: { $in: ['snack'] } } },
-  //       { $match: { diets: { $in: [diet] } } },
-  //       { $sample: { size: 1 } },
-  //     ])
-  //     .toArray();
-  //   menu.snack = snack[0];
-  // }
-
-  // let lunch = await db
-  //   .collection('meals')
-  //   .aggregate([
-  //     { $match: { type: { $in: ['lunch'] } } },
-  //     { $match: { diets: { $in: [diet] } } },
-  //     { $sample: { size: 1 } },
-  //   ])
-  //   .toArray();
-  // menu.lunch = lunch[0];
-
-  // if (numberOfMeals === 5) {
-  //   let snack2 = await db
-  //     .collection('meals')
-  //     .aggregate([
-  //       { $match: { type: { $in: ['snack'] } } },
-  //       { $match: { diets: { $in: [diet] } } },
-  //       { $sample: { size: 1 } },
-  //     ])
-  //     .toArray();
-  //   menu.snack2 = snack2[0];
-  // }
-
-  // let dinner = await db
-  //   .collection('meals')
-  //   .aggregate([
-  //     { $match: { type: { $in: ['dinner'] } } },
-  //     { $match: { diets: { $in: [diet] } } },
-  //     { $sample: { size: 1 } },
-  //   ])
-  //   .toArray();
-  // menu.dinner = dinner[0];
-  console.log('5');
-  const response = {
-    statusCode: 200,
-    body: menu ? JSON.stringify(menu) : {},
-  };
-  return response;
-
-  //--------------------------------------------------
-  // const result = await db
-  //   .collection('meals')
-  //   .aggregate([
-  //     { $match: { type: { $in: ['breakfast'] } } },
-  //     { $match: { diets: { $in: [diet] } } },
-  //     { $sample: { size: 1 } },
-  //   ])
-  //   .toArray();
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify(result[0]),
-  // };
-  // return response;
-  //--------------------------------------------------
-
-  // let diet = req.query.diet;
-  // let numberOfMeals = req.query.numberOfMeals;
-
-  // try {
-  //   let db_connect = await connectToDatabase();
-  //   let diet = 'vegetarian';
-  //   console.log(diet);
-  //   let menu = {};
-  //   let breakfast = await db_connect
-  //     .collection('meals')
-  //     .aggregate([
-  //       // { $match: { type: { $in: ['breakfast'] } } },
-  //       // { $match: { diets: { $in: [diet] } } },
-  //       { $sample: { size: 1 } },
-  //     ])
-  //     .toArray();
-  //   menu.breakfast = breakfast[0];
-  //   console.log(menu.breakfast);
-  //   // const res = await meals.find({});
-  //   const res = {
-  //     message: 'blablaaa',
-  //   };
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify(menu.breakfast),
-  //   };
-  // } catch (e) {
-  //   console.error(e);
-  //   return {
-  //     statusCode: 500,
-  //     body: JSON.stringify(e),
-  //   };
-  // }
+const handler = async (event) => {
+  console.log(1);
+  try {
+    const database = (await clientPromise).db('food');
+    const collection = database.collection('meals');
+    console.log(2);
+    const results = await collection
+      .aggregate([
+        { $match: { type: { $in: ['breakfast'] } } },
+        { $match: { diets: { $in: ['vegan'] } } },
+        { $sample: { size: 1 } },
+      ])
+      .toArray();
+    console.log(3 + JSON.stringify(results));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
 };
+
+module.exports = { handler };
+
+//-------------------------------------------------------------------start
+
+// const dbo = require('./db/conn');
+
+// exports.handler = async function (event, context, callback) {
+//   console.log('1');
+//   const params = JSON.parse(event.body);
+//   const numberOfMeals = params.meals;
+//   const diet = params.diet;
+//   console.log(numberOfMeals, diet);
+//   console.log('2');
+//   let menu = {};
+//   console.log('3');
+//   const db = await dbo.connectToDatabase();
+
+//   let breakfast = await db
+//     .collection('meals')
+//     .aggregate([
+//       { $match: { type: { $in: ['breakfast'] } } },
+//       { $match: { diets: { $in: [diet] } } },
+//       { $sample: { size: 1 } },
+//     ])
+//     .toArray();
+//   menu.breakfast = breakfast[0];
+//   console.log('menu.breakfast ' + menu.breakfast);
+
+// if (numberOfMeals === 5) {
+//   let snack = await db
+//     .collection('meals')
+//     .aggregate([
+//       { $match: { type: { $in: ['snack'] } } },
+//       { $match: { diets: { $in: [diet] } } },
+//       { $sample: { size: 1 } },
+//     ])
+//     .toArray();
+//   menu.snack = snack[0];
+// }
+
+// let lunch = await db
+//   .collection('meals')
+//   .aggregate([
+//     { $match: { type: { $in: ['lunch'] } } },
+//     { $match: { diets: { $in: [diet] } } },
+//     { $sample: { size: 1 } },
+//   ])
+//   .toArray();
+// menu.lunch = lunch[0];
+
+// if (numberOfMeals === 5) {
+//   let snack2 = await db
+//     .collection('meals')
+//     .aggregate([
+//       { $match: { type: { $in: ['snack'] } } },
+//       { $match: { diets: { $in: [diet] } } },
+//       { $sample: { size: 1 } },
+//     ])
+//     .toArray();
+//   menu.snack2 = snack2[0];
+// }
+
+// let dinner = await db
+//   .collection('meals')
+//   .aggregate([
+//     { $match: { type: { $in: ['dinner'] } } },
+//     { $match: { diets: { $in: [diet] } } },
+//     { $sample: { size: 1 } },
+//   ])
+//   .toArray();
+// menu.dinner = dinner[0];
+// console.log('5');
+// const response = {
+//   statusCode: 200,
+//   body: menu ? JSON.stringify(menu) : {},
+// };
+// return response;
+
+//--------------------------------------------------konec
+// const result = await db
+//   .collection('meals')
+//   .aggregate([
+//     { $match: { type: { $in: ['breakfast'] } } },
+//     { $match: { diets: { $in: [diet] } } },
+//     { $sample: { size: 1 } },
+//   ])
+//   .toArray();
+// const response = {
+//   statusCode: 200,
+//   body: JSON.stringify(result[0]),
+// };
+// return response;
+//--------------------------------------------------
+
+// let diet = req.query.diet;
+// let numberOfMeals = req.query.numberOfMeals;
+
+// try {
+//   let db_connect = await connectToDatabase();
+//   let diet = 'vegetarian';
+//   console.log(diet);
+//   let menu = {};
+//   let breakfast = await db_connect
+//     .collection('meals')
+//     .aggregate([
+//       // { $match: { type: { $in: ['breakfast'] } } },
+//       // { $match: { diets: { $in: [diet] } } },
+//       { $sample: { size: 1 } },
+//     ])
+//     .toArray();
+//   menu.breakfast = breakfast[0];
+//   console.log(menu.breakfast);
+//   // const res = await meals.find({});
+//   const res = {
+//     message: 'blablaaa',
+//   };
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(menu.breakfast),
+//   };
+// } catch (e) {
+//   console.error(e);
+//   return {
+//     statusCode: 500,
+//     body: JSON.stringify(e),
+//   };
+// }
+
 // recordRoutes.route('/record').get(async function (req, res) {
 //   let db_connect = dbo.getDb();
 //   let diet = req.query.diet;
